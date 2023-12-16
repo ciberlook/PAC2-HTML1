621b3630f74f55006bf4bcdbee9bc0ec4397d796
Alberto Marín Mendoza <amarinme@uoc.edu>
2023-12-16 12:56:23 +0100

    Documentacion

---
+++ dist/index.39e2de79.js
@@ -0,0 +1,10 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+/**
+ * Write any other JavaScript below
+ */const t=document.querySelector(".sandwich"),e=document.querySelector(".menu-principal");t.addEventListener("click",t=>{"bar1"===t.target.classList[0]||"bar2"===t.target.classList[0]||"bar3"===t.target.classList[0]?t.target.parentElement.classList.toggle("change"):t.target.classList.toggle("change"),e.classList.toggle("menu-principal--hidden")},!1);//# sourceMappingURL=index.39e2de79.js.map
+
+//# sourceMappingURL=index.39e2de79.js.map
