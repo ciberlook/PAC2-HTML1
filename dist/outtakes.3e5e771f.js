4d336ee6f6151eb87c3c7ef875cc39bae47fee49
Alberto Marín Mendoza <amarinme@uoc.edu>
2024-01-03 16:07:20 +0100

    carga de videos async

---
+++ dist/outtakes.3e5e771f.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", ()=>{
+    const carrusel = document.querySelector(".carrusel");
+    carrusel.innerHTML = '<iframe loading="lazy" src="https://www.youtube.com/embed/8pN96nOt31g?si=Cw07FlkWawmtdwpu" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>    <iframe loading="lazy" src="https://www.youtube.com/embed/IJYNJsEB2uk?si=iKi3FYvdjV3tFV8L" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>    <iframe loading="lazy" src="https://www.youtube.com/embed/q6iI1iSdXGM?si=kv4-m5FjwQwaWuGI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+}, false);
+
+//# sourceMappingURL=outtakes.3e5e771f.js.map
