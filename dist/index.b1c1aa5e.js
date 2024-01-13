faa7d90577ba5d4180f08c66d26f4987e20bece4
Alberto Marín Mendoza <amarinme@uoc.edu>
2024-01-13 18:53:15 +0100

    Fin

--- dist/index.b1c1aa5e.js
+++
@@ -1,9 +0,0 @@
-const mButton = document.querySelector(".sandwich");
-const menu = document.querySelector(".menu-principal");
-mButton.addEventListener("click", (e)=>{
-    if (e.target.classList[0] === "bar1" || e.target.classList[0] === "bar2" || e.target.classList[0] === "bar3") e.target.parentElement.classList.toggle("change");
-    else e.target.classList.toggle("change");
-    menu.classList.toggle("menu-principal--hidden");
-}, false);
-
-//# sourceMappingURL=index.b1c1aa5e.js.map
---
+++ dist/index.bea0cf63.js
@@ -0,0 +1,3 @@
+const t=document.querySelector(".sandwich"),e=document.querySelector(".menu-principal");t.addEventListener("click",t=>{"bar1"===t.target.classList[0]||"bar2"===t.target.classList[0]||"bar3"===t.target.classList[0]?t.target.parentElement.classList.toggle("change"):t.target.classList.toggle("change"),e.classList.toggle("menu-principal--hidden")},!1);//# sourceMappingURL=index.bea0cf63.js.map
+
+//# sourceMappingURL=index.bea0cf63.js.map
